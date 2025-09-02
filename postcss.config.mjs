7487845c0f8c403291e48e328203e213706c65a8
ABHINAV RAJ <92502850+probably-ABHINAV@users.noreply.github.com>
2025-09-02 13:14:09 +0530

    Fix CSS and Tailwind configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,20 +1,9 @@
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { 
-      cssnano: {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: false,
-        }]
-      } 
-    } : {}),
   },
 }
 
